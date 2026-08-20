b74fa80b5dd6c95ca831c0e5a38368d3c12b942d
Artem Shubin <archifyrr@gmail.com>
2026-08-20 19:02:38 +0700

    Prepare Android 1.6.0 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.astrosmap.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
     }
 
     buildTypes {
