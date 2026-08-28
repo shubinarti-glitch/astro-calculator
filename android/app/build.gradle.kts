8e5d98b87523b59be69c704e3a02bad2208ceb18
Artem Shubin <archifyrr@gmail.com>
2026-08-28 15:18:05 +0700

    Bump Android version to 1.7.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.astrosmap.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.7.1"
     }
 
     buildTypes {
